107cefd6fa49cc9961e7b193cd8e5917f85c4975
roshankumar2303 <58880369+roshankumar2303@users.noreply.github.com>
2021-02-23 18:23:51 +0530

    Added routing
    Created components home, store, about and support and added routing to these components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
+import { SupportComponent } from './support/support.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:"", redirectTo:"/home", pathMatch:"full"},
+    {path:"home", component:HomeComponent},
+    {path:"about", component:AboutComponent},
+    {path:"store", component:StoreComponent},
+    {path:"support", component:SupportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
+import { AboutComponent } from './about/about.component';
+import { SupportComponent } from './support/support.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StoreComponent,
+    AboutComponent,
+    SupportComponent
   ],
   imports: [
     BrowserModule,
