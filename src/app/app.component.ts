42e733e9d4bed0b95f605448e9fbb660f668a7ee
roshankumar2303 <58880369+roshankumar2303@users.noreply.github.com>
2021-02-23 21:12:15 +0530

    Animated hamburger icon
    Added animated hamburger icon for the collapsed navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Training-Project';
+    title = 'Training-Project';
+
+    hamToggle: boolean = false;
+    changeHamStatus() {
+        // The collapsed navbar is open if hamToggle is true
+        this.hamToggle = !this.hamToggle;
+    }
 }
